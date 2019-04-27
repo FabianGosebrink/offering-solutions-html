4efb64d20e21f80acd049280891cad7e5ad8cd81
FabianGosebrink <fabian.gosebrink@outlook.com>
2019-04-27 13:06:42 +0200

    added search

--- homepage/static/js/search.js
+++ homepage/static/js/search.js
@@ -1,45 +1,43 @@
 (function($) {
-    var $wn = $(window);
-    ('use strict');
-    $(window).on('load', function() {
-        function getFormattedDate(date) {
-            var todayTime = new Date(date);
-            var month = todayTime.getMonth() + 1;
-            var day = todayTime.getDate();
-            var year = todayTime.getFullYear();
-            return day + '.' + month + '.' + year;
-        }
-console.log("dadsadas");
-        $.ajax({
-            url: 'https://offering.solutions/blog/search.json',
-            success: function(result) {
-                var obj = JSON.parse(result);
-                var firstThree = obj.slice(0, 3);
-                firstThree.forEach(function(element) {
-                    var html =
-                        "<div class='col-pd-3 single-post'>" +
-                        "<div class='row post-header filter'>" +
-                        "<img src='images/blog-preview.jpg' alt=''>" +
-                        '</div>' +
-                        "<div class='row post-body'>" +
-                        "<p class='date'>" +
-                        getFormattedDate(element.date) +
-                        '</p>' +
-                        "<h3><a href='" +
-                        element.url +
-                        "'>" +
-                        element.title +
-                        '</a></h3>' +
-                        "<div class='seperator'></div>" +
-                        "<a href='" +
-                        element.url +
-                        "'>Read More <i class='fa fa-long-arrow-right' aria-hidden='true'></i></a>" +
-                        '</div>' +
-                        '</div>';
+  ('use strict');
+  var allblogs = [];
 
-                    $('#blogPosts').append(html);
-                }, this);
-            }
-        });
+  $('#blogsearch').keyup(function() {
+    var currentValue = $(this)
+      .val()
+      .toLowerCase();
+
+    var openingHtml = '<ul>';
+    var closingHtml = '</ul>';
+    resultListItems = [];
+
+    allblogs.forEach(element => {
+      if (element.title.toLowerCase().includes(currentValue)) {
+        var html =
+          '<li>' +
+          "<a href='" +
+          element.uri +
+          "'>" +
+          element.title +
+          '</a></li>';
+
+        resultListItems.push(html);
+      }
     });
+    console.log(resultListItems.join(''));
+    var result = openingHtml + resultListItems.join('') + closingHtml;
+
+    $('#blogPosts').html(result);
+  });
+
+  $(window).on('load', function() {
+    console.log('dadsadas');
+    $.ajax({
+      url: '/index.json',
+      success: function(result) {
+        allblogs = result;
+        console.log(result);
+      }
+    });
+  });
 })(jQuery);
