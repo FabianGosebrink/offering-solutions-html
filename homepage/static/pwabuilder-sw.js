0efb772b39d38f1228830ec39c3d51dd53236a1b
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2020-01-11 14:06:01 +0100

    removed not needed lines

--- homepage/static/pwabuilder-sw.js
+++ homepage/static/pwabuilder-sw.js
@@ -3,7 +3,7 @@
 const CACHE = "pwabuilder-offline";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "index.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+const offlineFallbackPage = "404.html";
 
 // Install stage sets up the index page (home page) in the cache and opens a new cache
 self.addEventListener("install", function (event) {
@@ -11,12 +11,7 @@ self.addEventListener("install", function (event) {
 
   event.waitUntil(
     caches.open(CACHE).then(function (cache) {
-      console.log("Cached offline page during install");
-
-      if (offlineFallbackPage === "ToDo-replace-this-name.html") {
-        return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
-      }
-      
+      console.log("Cached offline page during install");     
       return cache.add(offlineFallbackPage);
     })
   );
